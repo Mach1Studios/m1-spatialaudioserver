5fc4383c1407563bcb932a21adf2df5fd98edba6
Anton Tamakhin <snyypi@gmail.com>
2021-05-23 23:00:57 +0300

    minor: just staged all tested changes

--- koa-server/public/switcher.sh
+++ koa-server/public/switcher.sh
@@ -2,8 +2,9 @@
 
 # echo "lol you run me" >> /share/sound/test.txt
 
-# ffmpeg -analyzeduration 10M  -re -i /share/sound/m1-debug-visual.wav -strict -2 -c:a libopus -mapping_family 255 -f flv rtmp://127.0.0.1/live/play >> /share/sound/test.txt
-ffmpeg -analyzeduration 10M  -re -i /share/sound/$1 -strict -2 -c:a libopus -mapping_family 255 -f flv rtmp://127.0.0.1/live/play >> /share/sound/test.txt
+# ffmpeg -stream_loop 100 -analyzeduration 10M  -re -i /share/sound/m1-debug-visual.wav -strict -2 -c:a libopus -mapping_family 255 -f flv rtmp://127.0.0.1/live/play >> /share/sound/test.txt
+# ffmpeg -y -stream_loop -1 -i /share/sound/test.wav -af "channelmap=channel_layout=hexadecagonal" -c:a aac -ac 16 -b:a 2048k -f flv "rtmp://127.0.0.1:1935/live/play"
+ffmpeg -stream_loop -analyzeduration 10M  -re -i /share/sound/$1 -strict -2 -c:a libopus -mapping_family 255 -f flv rtmp://127.0.0.1/live/play >> /share/sound/test.txt
 
 echo "I'm a response in bash from NGINX, you try to run a sound file through FFmpeg. Sound file is:"
 echo "$1"
--- vue-front/src/components/Player.vue
+++ vue-front/src/components/Player.vue
@@ -1,20 +1,75 @@
 <template>
   <div>
+    <!-- <div class="wrapper">
+      <video autoplay="false" ref="player"></video>
+    </div> -->
     <vue-plyr>
-      <audio controls crossorigin playsinline>
-        <source
+      <audio controls crossorigin playsinline ref="player">
+        <!-- <source
             src="/m1-debug-shrtpt-m1spatial.wav"
             type="audio/wav"
-        />
+        /> -->
       </audio>
     </vue-plyr>
   </div>
 </template>
 
 <script>
+/* eslint-disable */
+import dashjs from 'dashjs';
+
+// const videoPlayer = document.createElement("video");
+// videoPlayer.setAttribute("class", "VideoPlayer");
+// videoPlayer.setAttribute("id", "videoPlayer");
+// videoPlayer.muted = true;
+
+// const POLLING_INTERVAL = 1000;
+const STABLE_BUFFER_TIME = 20;
+const BUFFER_TIME_AT_TOP_QUALITY = 20;
+const MANIFEST_LOAD_RETRY_INTERVAL = 50000;
+
+const DEFAULT_CLIENT_SETTINGS = {
+  streaming: {
+    useSuggestedPresentationDelay: false,
+    lowLatencyEnabled: false,
+    stableBufferTime: STABLE_BUFFER_TIME,
+    bufferTimeAtTopQualityLongForm: BUFFER_TIME_AT_TOP_QUALITY,
+    retryIntervals: {
+      MPD: MANIFEST_LOAD_RETRY_INTERVAL,
+    },
+    retryAttempts: {
+      MPD: 3,
+    },
+  },
+};
+
 export default {
-  name: 'Component',
-  mounted() {},
+  name: 'Player',
+  mounted() {
+    const audio = document.querySelector('audio');
+    const player = dashjs.MediaPlayer().create();
+
+    console.log(this.$refs.player, audio);
+    player.updateSettings(DEFAULT_CLIENT_SETTINGS);
+
+    const url = "https://livesim.dashif.org/dash/vod/testpic_2s/audio.mpd";
+    const localurl = 'http://localhost:8080/dash/play.mpd';
+    player.initialize(this.$refs.player, localurl, true);
+
+    player.on(dashjs.MediaPlayer.events.MANIFEST_LOADED, ({ data }) => {
+      console.log('loaded', data);
+
+      const audioAdaptationSet = data.Period.AdaptationSet_asArray.find((elem) => elem.contentType === 'audio');
+      const videoAdaptationSets = data.Period.AdaptationSet_asArray.filter((elem) => elem.contentType === 'video');
+      const numChannels = Number(audioAdaptationSet.Representation_asArray[0].AudioChannelConfiguration.value);
+
+      console.log(audioAdaptationSet, videoAdaptationSets, numChannels);
+    });
+
+    player.on(dashjs.MediaPlayer.events.ERROR, (error) => {
+      console.log('error', error);
+    });
+  },
 };
 </script>
 
@@ -24,5 +79,8 @@ export default {
   }
  .plyr__control:hover {
     background: transparent;
+  }
+  .hidden {
+    display: none;
   }
 </style>
