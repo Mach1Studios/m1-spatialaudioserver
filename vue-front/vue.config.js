fea0308f0e7e4f0aa6882afbd6c9c410c9e04b61
Anton Tamakhin <snyypi@gmail.com>
2021-07-06 21:28:53 +0300

    build: added new frontend dependency; fixed compilation error for icons components

--- vue-front/vue.config.js
+++ vue-front/vue.config.js
@@ -1,3 +1,16 @@
 module.exports = {
-  transpileDependencies: ['mach1spatial-decode']
-}
+  transpileDependencies: ['mach1spatial-decode'],
+  chainWebpack: (config) => {
+    config.module
+      .rule('vue')
+      .use('vue-loader')
+      .tap((options) => {
+        // eslint-disable-next-line
+        options.compilerOptions = {
+          ...options.compilerOptions,
+          isCustomElement: (tag) => tag.startsWith('ion-'),
+        };
+        return options;
+      });
+  },
+};
