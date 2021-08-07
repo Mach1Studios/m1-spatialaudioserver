afec59dfe553869e3139a62af5d08ee547a32b33
Anton Tamakhin <snyypi@gmail.com>
2021-08-08 00:38:24 +0300

    bugfix: updated api cors middleware

--- koa-server/configs/middleware/cors.js
+++ koa-server/configs/middleware/cors.js
@@ -1,18 +1,26 @@
+/* eslint-disable consistent-return */
 import _ from 'lodash';
 
 export default (options) => {
   const isEnabled = !_.get(options, 'disable', false);
 
+  const defaultHeaders = {
+    allowMethods: ['GET', 'HEAD', 'POST', 'PUT', 'DELETE', 'PATCH'],
+  };
+
   return async (ctx, next) => {
     const origin = ctx.get('Origin');
+    ctx.vary('Origin');
+
     if (!origin || !isEnabled) return next();
-
-    if (ctx.method === 'OPTIONS') {
-      throw new Error("Options doesn't support on this moment, full cors should implement...");
-    } else {
+    if (ctx.method !== 'OPTIONS') {
       ctx.set('Access-Control-Allow-Origin', origin);
-
       return next();
     }
+
+    ctx.set('Access-Control-Allow-Origin', origin);
+    ctx.set('Access-Control-Allow-Methods', defaultHeaders.allowMethods.join(','));
+
+    ctx.status = 204;
   };
 };
