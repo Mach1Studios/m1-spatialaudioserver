33e2f31aa82c7a1ab45182f34591db3d26fbbe40
Anton Tamakhin <snyypi@gmail.com>
2021-09-04 19:14:44 +0300

    frontend: created default immput component for forms

---
+++ vue-front/src/components/form/Input.vue
@@ -0,0 +1,45 @@
+<template lang="html">
+  <div class="field label border">
+    <input :name="name" :type="type" :value="value" @focus="select" @blur="select" autocomplete="off">
+    <label v-show="plaseholder" :class="{ active: focused }">{{plaseholder}}</label>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FormInput',
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    type: {
+      type: String,
+      required: true,
+      default: 'text',
+    },
+    value: {
+      type: String,
+    },
+    plaseholder: {
+      type: String,
+    },
+  },
+  data() {
+    return { focused: false };
+  },
+  methods: {
+    select({ target: { name }, type }) {
+      console.log(name, type);
+      if (type === 'focus') {
+        this.focused = true;
+      } else if (type === 'blur' && this.value === '') {
+        this.focused = false;
+      }
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
