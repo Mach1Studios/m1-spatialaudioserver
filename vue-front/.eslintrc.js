9efb001d4011e4aca93e337b05e3bafd86cccbb0
Anton Tamakhin <snyypi@gmail.com>
2021-09-11 01:03:22 +0300

    frontend: reverted header changes for the current stage

--- vue-front/.eslintrc.js
+++ vue-front/.eslintrc.js
@@ -17,5 +17,6 @@ module.exports = {
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'max-len': ['warn', { code: 200 }],
     'no-param-reassign': ['error', { props: true, ignorePropertyModificationsFor: ['store', 'state'] }],
+    'import/no-cycle': 'warn'
   }
 };
