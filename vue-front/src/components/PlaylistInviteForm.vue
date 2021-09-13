65ec8ce92c3de3c116dda245b397b5fd1e545f4f
Anton Tamakhin <snyypi@gmail.com>
2021-09-13 18:06:13 +0300

    frontend: updated playlist form [after draft testing]

--- vue-front/src/components/PlaylistInviteForm.vue
+++ vue-front/src/components/PlaylistInviteForm.vue
@@ -1,27 +1,68 @@
 <template>
-  <div class="large-width invite">
-    <FormSelect name="" placeholder="Role"/>
-    <FormButton title="Invite" icon="send" @click="send()"/>
+  <div class="invite">
+    <FormSelect name="" placeholder="" :options="unbindedItems" @change="addItem"/>
+    <table class="list-table border">
+      <tbody>
+        <tr v-for="item in bindedItems" :key="item">
+          <td>
+            <p class="medium-text">{{item.number}}</p>
+          </td>
+          <td class="small-width">
+            <p class="medium-text">{{item.name}}</p>
+          </td>
+          <td>
+            <nav class="right-align">
+              <button class="border round transparent-border"  @click="del(item.id)">
+                <i class="material-icons">delete</i>
+              </button>
+            </nav>
+          </td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
-// import { mapActions } from 'vuex';
+import { mapState, mapActions } from 'vuex';
+import _ from 'lodash';
 
-import FormButton from './Form/Button.vue';
 import FormSelect from './Form/Select.vue';
 
 export default {
   name: 'PlaylistInviteForm',
   components: {
-    FormButton,
     FormSelect,
   },
+  props: {
+    playlist: Object,
+    path: String,
+    items: {
+      type: Array,
+    },
+  },
+  computed: {
+    ...mapState({
+      tracks: (state) => state.tracks.items,
+    }),
+    bindedItems() {
+      return _.filter(this.items, ({ id }) => this.playlist[this.path].indexOf(id) !== -1);
+    },
+    unbindedItems() {
+      return _.filter(this.items, ({ id }) => this.playlist[this.path].indexOf(id) === -1);
+    },
+  },
   data() {
-    return {
-    };
+    return {};
   },
   methods: {
+    ...mapActions('playlists', ['addItemToPlaylist', 'removeItemFromPlaylist']),
+    del(itemId) {
+      this.removeItemFromPlaylist({ id: this.playlist.id, [this.path]: _.xor(this.playlist[this.path], [itemId]) });
+    },
+    addItem(event) {
+      this.addItemToPlaylist({ id: this.playlist.id, [this.path]: _.union(this.playlist[this.path], [event.target.value]) });
+    },
   },
 };
 </script>
