eec848a9ccb48dfab3b42d99eb82c3c3c891a492
Anton Tamakhin <snyypi@gmail.com>
2021-10-10 16:58:12 +0300

    api: removed unused method

--- koa-server/api/auth.js
+++ koa-server/api/auth.js
@@ -5,31 +5,6 @@ import service from './services/encryption';
 
 export default {
   /**
-   * Checking and returning current user session: if exist return 200 else empty body and 204.
-   * Removing session object if a session was corrupted
-   * @param  {Object}  ctx  the default koa context whose encapsulates
-   *                          node's request and response objects into a single object
-   */
-  async get(ctx) {
-    const { id } = ctx.params;
-    if (id !== 'profile') ctx.throw(404);
-
-    const { user } = ctx.session;
-    const userId = _.get(user, 'id');
-
-    ctx.status = userId ? 200 : 204;
-    ctx.body = { user };
-
-    if (userId) {
-      const profile = await ctx.redis.hget(`user:${userId}`, 'id');
-
-      if (_.isNull(profile)) {
-        ctx.session = null;
-        ctx.throw(403, 'Session expired');
-      }
-    }
-  },
-  /**
    * Authenticating user by login (it can be nickname for email) and password;
    * if login success adding user profile into session
    * @param  {Object}  ctx  the default koa context whose encapsulates
