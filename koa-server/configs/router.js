bf34663616dc327703316933ff7936ec02ddb9c4
Anton Tamakhin <snyypi@gmail.com>
2021-10-12 19:40:27 +0300

    api: fixed routes [PUT, DELETE]

--- koa-server/configs/router.js
+++ koa-server/configs/router.js
@@ -12,7 +12,7 @@ const router = new Router({ prefix: '/v1' });
 // Authorization route
 router
   .post('/auth', auth.login)
-  .del('/auth', auth.validate, auth.logout);
+  .del('/auth/logout', auth.validate, auth.logout);
 
 // User profile route
 router
@@ -23,15 +23,15 @@ router
 router
   .get('/playlists', playlists.list)
   .post('/playlists', auth.validate, playlists.create)
-  .put('/playlists', auth.validate, playlists.update)
-  .del('/playlists', auth.validate, playlists.remove);
+  .put('/playlists/:id', auth.validate, playlists.update)
+  .del('/playlists/:id', auth.validate, playlists.remove);
 
 // Tracks route
 router
   .get('/tracks/:id', tracks.get)
   .get('/tracks', tracks.list)
-  .put('/tracks', auth.validate, tracks.update)
-  .del('/tracks', auth.validate, tracks.remove);
+  .put('/tracks/:id', auth.validate, tracks.update)
+  .del('/tracks/:id', auth.validate, tracks.remove);
 
 // Uploader
 router.post('/upload', auth.validate, upload.save);
@@ -39,7 +39,7 @@ router.post('/upload', auth.validate, upload.save);
 // User route
 router
   .get('/users', auth.validate, users.list)
-  .post('/users', auth.validate, playlists.create)
-  .del('/users', auth.validate, playlists.remove);
+  .post('/users', auth.validate, users.create)
+  .del('/users/:id', auth.validate, users.remove);
 
 export default router;
