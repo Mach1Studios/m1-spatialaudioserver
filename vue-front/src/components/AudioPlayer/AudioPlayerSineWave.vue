fd0a93ec52143fcc25bd577129de5185d34006d0
Anton Tamakhin <snyypi@gmail.com>
2022-01-11 21:59:07 +0300

    frontend: fixed visualization for L/R channels [draft]

--- vue-front/src/components/AudioPlayer/AudioPlayerSineWave.vue
+++ vue-front/src/components/AudioPlayer/AudioPlayerSineWave.vue
@@ -22,7 +22,7 @@ export default {
   data() {
     return {
       animationFrame: null,
-      bufferSize: 128,
+      bufferSize: 64,
       frame: {
         sliceWidth: 2,
         sliceHeight: 2,
@@ -38,6 +38,10 @@ export default {
         left: null,
         right: null,
       },
+
+      // DEBUG: animation
+      duration: 60 * 1,
+      start: 0,
     };
   },
 
@@ -67,7 +71,7 @@ export default {
     this.container.width = this.$refs[id].width;
 
     this.frame.sliceHeight = this.container.height / 256;
-    this.frame.sliceWidth = this.container.width / this.bufferSize;
+    this.frame.sliceWidth = (this.container.width * 9) / (this.bufferSize * 10);
 
     // console.log('slice=', this.frame.sliceHeight);
 
@@ -86,19 +90,50 @@ export default {
         this.provider.ctx.beginPath();
 
         let x = 0;
-        this.provider.ctx.moveTo(x, 24);
+        let step = 0;
+        // let y = 0;
+        //
+        const amplitude = 10;
+        const frequency = 10;
+        // const step = 4;
+        this.provider.ctx.moveTo(x, 25);
+        this.provider.ctx.lineTo(x = width / 20, height / 2);
         _.each(audioBuffer, (value) => {
-          this.provider.ctx.lineTo(x, value * this.frame.sliceHeight);
+          // this.provider.ctx.lineTo(x, value * this.frame.sliceHeight);
+          //
+          // x += this.frame.sliceWidth;
 
+          // this.provider.ctx.arc(x + value / 2, height, width / 2, Math.PI, 0);
+          const offsetY = step * amplitude * Math.sin((value - 100) * frequency * step);
+
+          // console.log(offsetY);
+          this.provider.ctx.lineTo(x, height / 2 + offsetY);
           x += this.frame.sliceWidth;
+
+          if (x < width / 2) {
+            step += 0.01;
+          } else {
+            step -= 0.01;
+          }
         });
+
+        // while (x < width) {
+        //   y = height / 2 + amplitude * Math.sin((x + 4) / frequency);
+        //   this.provider.ctx.lineTo(x, y);
+        //   x += 4;
+        // }
+        // this.provider.ctx.lineTo(x = (width * 19) / 20, height / 2);
+
         this.provider.ctx.lineTo(width, height / 2);
         this.provider.ctx.stroke();
         this.provider.ctx.closePath();
       };
       const animate = () => {
-        this.animationFrame = requestAnimationFrame(animate);
-
+        this.start += 1;
+        // console.log(this.channelsBuffer.left);
+        // if (this.duration === this.start) {
+        //   debugger;
+        // }
         const { height, width } = this.container;
         this.provider.ctx.clearRect(0, 0, width, height);
 
@@ -113,7 +148,9 @@ export default {
         // this.provider.ctx.miterLimit = 256;
 
         drawLine(this.channelsBuffer.left);
-        drawLine(this.channelsBuffer.right, '#4B3C53');
+        // drawLine(this.channelsBuffer.right, '#4B3C53');
+        //
+        this.animationFrame = requestAnimationFrame(animate);
       };
 
       if (this.isActiveStream && this.analyser[this.leftChannel] && this.analyser[this.rightChannel]) {
@@ -135,6 +172,8 @@ export default {
 <style lang="scss" scoped>
   canvas {
     background: transparent;
+
+    border: 1px solid white;
   }
 
   canvas .visualizer {
