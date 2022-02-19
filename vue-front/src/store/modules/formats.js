c74a7a25615453f60e572239e09e412522afbd82
Anton Tamakhin <snyypi@gmail.com>
2022-02-19 22:48:33 +0300

    frontend: minor changes for store modules

--- vue-front/src/store/modules/formats.js
+++ vue-front/src/store/modules/formats.js
@@ -2,9 +2,9 @@ import _ from 'lodash';
 
 const defaultState = () => ({
   items: [
-    { id: 'Stereo', name: 'Stereo', type: 'standart' },
-    { id: 'Stereo_Cinema', name: 'Stereo_Cinema', type: 'standart' },
-    { id: 'LCR', name: 'LCR', type: 'standart' },
+    { id: 'Stereo', name: 'Stereo', type: 'standart', numberOfChannels: 2 },
+    { id: 'Stereo_Cinema', name: 'Stereo_Cinema', type: 'standart', numberOfChannels: 2 },
+    { id: 'LCR', name: 'LCR', type: 'standart', numberOfChannels: 3 },
     { id: 'M1Horizon', name: 'M1Horizon (Mach1 Horizon/Quad)', type: 'mach1' },
     { id: 'M1HorizonS', name: 'M1Horizon+S (Mach1 Horizon/Quad)', type: 'mach1' },
     { id: 'M1HorizonPairs', name: 'M1HorizonPairs (Mach1 Horizon/Quad-Binaural)', type: 'mach1' },
@@ -12,30 +12,30 @@ const defaultState = () => ({
     { id: 'M1SpatialS', name: 'M1Spatial+S (Mach1 Spatial)', type: 'mach1' },
     { id: 'M1SpatialPairs', name: 'M1SpatialPairs (Mach1 Spatial Pairs)', type: 'mach1' },
     { id: 'M1SpatialFaces', name: 'M1SpatialFaces', type: 'mach1' },
-    { id: 'FiveOh', name: 'FiveOh', type: 'standart' },
-    { id: 'FiveOneFilm', name: 'FiveOneFilm (Pro Tools default/C|24)', type: 'standart' },
-    { id: 'FiveOneFilm_Cinema', name: 'FiveOneFilm_Cinema (Pro Tools default/C|24)', type: 'standart' },
-    { id: 'FiveOneSmpte', name: 'FiveOneSmpte (SMPTE/ITU for Dolby Digital (AC3)', type: 'standart' },
-    { id: 'FiveOneDts', name: 'FiveOneDts (DTS)', type: 'standart' },
-    { id: 'SixOh', name: 'SixOh', type: 'standart' },
-    { id: 'SevenOnePt', name: 'SevenOnePt (Pro Tools default)', type: 'standart' },
-    { id: 'SevenOnePt_Cinema', name: 'SevenOnePt_Cinema (Pro Tools default)', type: 'standart' },
-    { id: 'SevenZero_Cinema', name: 'SevenZero_Cinema (Pro Tools default)', type: 'standart' },
-    { id: 'SevenOneSDDS', name: 'SevenOneSDDS (Sony SDDS)', type: 'standart' },
-    { id: 'SevenZeroSDDS', name: 'SevenZeroSDDS (Sony SDDS)', type: 'standart' },
-    { id: 'FiveOneTwo', name: 'FiveOneTwo (Film/Pro Tools default)', type: 'standart' },
-    { id: 'FiveZeroTwo', name: 'FiveZeroTwo (Film/Pro Tools default)', type: 'standart' },
-    { id: 'FiveOneFour', name: 'FiveOneFour (Film/Pro Tools default)', type: 'standart' },
-    { id: 'FiveZeroFour', name: 'FiveZeroFour (Film/Pro Tools default)', type: 'standart' },
-    { id: 'SevenOneTwo', name: 'SevenOneTwo (Film/Pro Tools default)', type: 'standart' },
-    { id: 'SevenZeroTwo', name: 'SevenZeroTwo (Film/Pro Tools default)', type: 'standart' },
-    { id: 'SevenOneFour', name: 'SevenOneFour (Film/Pro Tools default)', type: 'standart' },
-    { id: 'SevenZeroFour', name: 'SevenZeroFour (Film/Pro Tools default)', type: 'standart' },
-    { id: 'SevenOneTwo', name: 'SevenOneTwo (SMPTE)', type: 'standart' },
-    { id: 'SevenZeroTwo', name: 'SevenZeroTwo (SMPTE)', type: 'standart' },
-    { id: 'DolbyAtmosSevenOneTwo', name: 'DolbyAtmosSevenOneTwo', type: 'standart' },
-    { id: 'NineOne', name: 'NineOne', type: 'standart' },
-    { id: 'NineZero', name: 'NineZero', type: 'standart' },
+    { id: 'FiveOh', name: 'FiveOh', type: 'standart', numberOfChannels: 5 },
+    { id: 'FiveOneFilm', name: 'FiveOneFilm (Pro Tools default/C|24)', type: 'standart', numberOfChannels: 6 },
+    { id: 'FiveOneFilm_Cinema', name: 'FiveOneFilm_Cinema (Pro Tools default/C|24)', type: 'standart', numberOfChannels: 6 },
+    { id: 'FiveOneSmpte', name: 'FiveOneSmpte (SMPTE/ITU for Dolby Digital (AC3)', type: 'standart', numberOfChannels: 6 },
+    { id: 'FiveOneDts', name: 'FiveOneDts (DTS)', type: 'standart', numberOfChannels: 6 },
+    { id: 'SixOh', name: 'SixOh', type: 'standart', numberOfChannels: 6 }, // TODO: need to check number of channels for this type of format
+    { id: 'SevenOnePt', name: 'SevenOnePt (Pro Tools default)', type: 'standart', numberOfChannels: 8 },
+    { id: 'SevenOnePt_Cinema', name: 'SevenOnePt_Cinema (Pro Tools default)', type: 'standart', numberOfChannels: 8 },
+    { id: 'SevenZero_Cinema', name: 'SevenZero_Cinema (Pro Tools default)', type: 'standart', numberOfChannels: 7 },
+    { id: 'SevenOneSDDS', name: 'SevenOneSDDS (Sony SDDS)', type: 'standart', numberOfChannels: 8 },
+    { id: 'SevenZeroSDDS', name: 'SevenZeroSDDS (Sony SDDS)', type: 'standart', numberOfChannels: 7 },
+    { id: 'FiveOneTwo', name: 'FiveOneTwo (Film/Pro Tools default)', type: 'standart', numberOfChannels: 11 },
+    { id: 'FiveZeroTwo', name: 'FiveZeroTwo (Film/Pro Tools default)', type: 'standart', numberOfChannels: 11 }, // FIXME: finished here
+    { id: 'FiveOneFour', name: 'FiveOneFour (Film/Pro Tools default)', type: 'standart', numberOfChannels: 2 },
+    { id: 'FiveZeroFour', name: 'FiveZeroFour (Film/Pro Tools default)', type: 'standart', numberOfChannels: 2 },
+    { id: 'SevenOneTwo', name: 'SevenOneTwo (Film/Pro Tools default)', type: 'standart', numberOfChannels: 2 },
+    { id: 'SevenZeroTwo', name: 'SevenZeroTwo (Film/Pro Tools default)', type: 'standart', numberOfChannels: 2 },
+    { id: 'SevenOneFour', name: 'SevenOneFour (Film/Pro Tools default)', type: 'standart', numberOfChannels: 2 },
+    { id: 'SevenZeroFour', name: 'SevenZeroFour (Film/Pro Tools default)', type: 'standart', numberOfChannels: 2 },
+    { id: 'SevenOneTwo', name: 'SevenOneTwo (SMPTE)', type: 'standart', numberOfChannels: 2 },
+    { id: 'SevenZeroTwo', name: 'SevenZeroTwo (SMPTE)', type: 'standart', numberOfChannels: 2 },
+    { id: 'DolbyAtmosSevenOneTwo', name: 'DolbyAtmosSevenOneTwo', type: 'standart', numberOfChannels: 2 },
+    { id: 'NineOne', name: 'NineOne', type: 'standart', numberOfChannels: 2 },
+    { id: 'NineZero', name: 'NineZero', type: 'standart', numberOfChannels: 2 },
     { id: 'ACNSN3D', name: 'ACNSN3D', type: 'ambisonic' },
     { id: 'FuMa', name: 'FuMa', type: 'ambisonic' },
     { id: 'TBE', name: 'TBE', type: 'ambisonic' },
@@ -50,18 +50,19 @@ const defaultState = () => ({
     { id: 'ACNSN3DmaxRE5oa', name: 'ACNSN3DmaxRE5oa', type: 'ambisonic' },
     { id: 'ACNSN3DmaxRE6oa', name: 'ACNSN3DmaxRE6oa', type: 'ambisonic' },
     { id: 'ACNSN3DmaxRE7oa', name: 'ACNSN3DmaxRE7oa', type: 'ambisonic' },
-    { id: 'CH_16', name: '16.0', type: 'standart' },
+    { id: 'CH_16', name: '16.0', type: 'standart', numberOfChannels: 16 },
   ],
   item: {
     input: null,
     output: null,
+    numberOfChannels: null,
   },
   defaultInput: null,
   defaultOutput: null,
 });
 
 const actions = {
-  setFormatAsDefault({ commit, state }, data) {
+  updateDefaultFormats({ commit, state }, data) {
     const { input, output } = data;
 
     const isInputExist = (_.isString(input) && _.findIndex(state.items, { id: input })) || _.isNull(input);
@@ -74,14 +75,48 @@ const actions = {
       commit('setDefaultOutput', output);
     }
   },
+  validateAudio({ commit, dispatch, state }, track) {
+    commit('loader', { description: 'Checking number of channels' }, { root: true });
+
+    const context = new (window.AudioContext || window.webkitAudioContext)();
+    const reader = new FileReader();
+
+    reader.onload = (e) => {
+      const { result } = e.target;
+
+      context.decodeAudioData(result, (buffer) => {
+        const source = context.createBufferSource();
+        source.connect(context.destination);
+        source.buffer = buffer;
+
+        console.log(!_.isNull(state.item.numberOfChannels) && e.item.numberOfChannels !== buffer.numberOfChannels);
+        if (!_.isNull(state.item.numberOfChannels) && e.item.numberOfChannels !== buffer.numberOfChannels) {
+          dispatch('toast', { event: { message: 'The selected tracks have a different number of channels and cannot be uploaded together' } }, { root: true });
+        } else {
+          commit('setNumberOfChannels', buffer.numberOfChannels);
+        }
+
+        commit('loader', { enable: false }, { root: true });
+      });
+    };
+    reader.readAsArrayBuffer(track);
+  },
 };
 
 const getters = {
   inputFormats(store) {
-    return store.items.filter(({ id }) => id !== store.item.output);
+    console.log(store.item.numberOfChannels);
+    // return store.items.filter(({ id }) => id !== store.item.output);
+    return store.items.filter(({ numberOfChannels }) => {
+      console.log(numberOfChannels, store.item.numberOfChannels);
+      return numberOfChannels === store.item.numberOfChannels;
+    });
   },
   outputFormats(store) {
     return store.items.filter(({ id, type }) => type === 'mach1' && id !== store.item.input);
+  },
+  validated(store) {
+    return !_.isNull(store.item.numberOfChannels);
   },
 };
 
@@ -91,6 +126,12 @@ const mutations = {
   },
   setDefaultOutput(store, id) {
     store.defaultOutput = id;
+  },
+  setItemValidation(store, value) {
+    store.item.validated = value;
+  },
+  setNumberOfChannels(store, value) {
+    store.item.numberOfChannels = value;
   },
 };
 export default {
