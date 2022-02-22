e295caad713fcf6f7f9224575a2b86ae003b6177
Anton Tamakhin <snyypi@gmail.com>
2022-02-22 20:39:17 +0300

    build: updated eslint rules

--- vue-front/.eslintrc.js
+++ vue-front/.eslintrc.js
@@ -1,8 +1,6 @@
 module.exports = {
   root: true,
-  env: {
-    node: true,
-  },
+  env: { node: true },
   extends: [
     'plugin:vue/vue3-essential',
     '@vue/airbnb',
@@ -13,10 +11,11 @@ module.exports = {
     requireConfigFile: false,
   },
   rules: {
+    'import/no-cycle': 'warn',
+    'max-len': ['warn', { code: 200 }],
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'max-len': ['warn', { code: 200 }],
     'no-param-reassign': ['error', { props: true, ignorePropertyModificationsFor: ['store', 'state'] }],
-    'import/no-cycle': 'warn',
+    'object-curly-newline': ['error', { ObjectExpression: { consistent: true, multiline: true, minProperties: 5 } }],
   },
 };
